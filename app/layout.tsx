965ca102809dbc2e2ace75f53df181c1f6a4b30a
Rajat Sharma <lunasunkaiser@gmail.com>
2025-05-10 03:06:59 +0530

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
